e318cef57351c48a347f8d14959d388c4ba03fd8
Luis <lumihernadezma@ittepic.edu.mx>
2021-09-27 13:49:58 -0700

    tercer commit
    Se agregaron funciones abrir nota y objeto nota

--- app/src/main/java/mx/ittepic/tepic/ladm_u1_practica2_archivos_memoria_i_e/MainActivity.kt
+++ app/src/main/java/mx/ittepic/tepic/ladm_u1_practica2_archivos_memoria_i_e/MainActivity.kt
@@ -2,10 +2,15 @@ package mx.ittepic.tepic.ladm_u1_practica2_archivos_memoria_i_e
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import mx.tecnm.tepic.ladm_u1_practica1_layoutsymaslayouts.EscalonarRecicler
 import mx.tecnm.tepic.ladm_u1_practica1_layoutsymaslayouts.ModeloRecilcer
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     private var escalalista = ArrayList<ModeloRecilcer>()
@@ -20,11 +25,24 @@ class MainActivity : AppCompatActivity() {
         adaptador = EscalonarRecicler(this,escalalista)
         rvEscalar.adapter = adaptador
 
-        iniciarEscalar()
+        abrirNotas()
     }
 
-    private fun iniciarEscalar() {
-        val imagen1 = ModeloRecilcer("PEPITO","Hola muy buenasmuy buenas el dia de hoy lome lomuy buenas el dia de hoy lome lo")
-        escalalista.add(imagen1)
+    private fun abrirNotas() {
+        //Ejemplo de abrir de un txt varias notas
+        try {
+            val archivo = BufferedReader(InputStreamReader(openFileInput("archivo.txt")))// Lee archivo separado por comas
+            archivo.forEachLine {
+                val listado = it.split(",") // crea arreglo linea a linea separado por comas
+                val Nota1:Nota = Nota() // Se crea objeto nota
+                val imagen1 = ModeloRecilcer(Nota1.titulo,Nota1.texto)// Se agrega nota al adaptador de recicler
+                escalalista.add(imagen1) // Se agrega Nota al Recicler
+            }
+        }catch (io : IOException){
+        }
+    }
+
+    private fun agregarNota() {
+
     }
 }
---
+++ app/src/main/java/mx/ittepic/tepic/ladm_u1_practica2_archivos_memoria_i_e/Nota.kt
@@ -0,0 +1,6 @@
+package mx.ittepic.tepic.ladm_u1_practica2_archivos_memoria_i_e
+
+class Nota {
+    var titulo = ""
+    var texto = ""
+}
