3b94d910b7e31cd6345e3c721843937d20197df7
Luis <lumihernadezma@ittepic.edu.mx>
2021-09-26 21:27:29 -0700

    first commit

---
+++ app/src/androidTest/java/mx/ittepic/tepic/ladm_u1_practica2_archivos_memoria_i_e/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package mx.ittepic.tepic.ladm_u1_practica2_archivos_memoria_i_e
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals(
+            "mx.ittepic.tepic.ladm_u1_practica2_archivos_memoria_i_e",
+            appContext.packageName
+        )
+    }
+}
---
+++ app/src/main/java/mx/ittepic/tepic/ladm_u1_practica2_archivos_memoria_i_e/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.ittepic.tepic.ladm_u1_practica2_archivos_memoria_i_e
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
